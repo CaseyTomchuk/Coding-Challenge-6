0bb13c67ca339c9e6cd347663d4fe2de237a60a2
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-02-09 20:08:47 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -15,3 +15,16 @@ function calculateSalesTax(amount, taxRate) { // Same structure as task 1
 }
 console.log(`Sales Tax: $${calculateSalesTax(100, 0.05)}`); // Sales Tax: $5
 console.log(`Sales Tax: $${calculateSalesTax(150, 0.1)}`); // Sales Tax: $15
+
+// Task 3: Arrow Function
+let calculateBonus = (salary, performanceRating) => { // Takes in the inputs salary, performanceRating
+    let bonusRatings = { //Creating an object to store the possible bonus ratings
+        "Excellent": 0.2,
+        "Good": 0.1,
+        "Average": 0.05
+    };
+    let bonus = salary * (bonusRatings[performanceRating]); // Multiplies the salary by the bonus, with the bonus defined by the performance rating
+    return (`Bonus: $${bonus}`);
+}
+console.log(calculateBonus(5000, "Excellent")); // Bonus: $1000
+console.log(calculateBonus(7500, "Average")); // Bonus: $375
