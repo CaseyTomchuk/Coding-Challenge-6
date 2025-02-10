f6c0dc4b113e9f0b02c1d2c014b1e33ca096ac4e
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-02-09 19:53:45 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -4,5 +4,14 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) { // Defining varia
     let profit = (sellingPrice - costPrice) * unitsSold; // Defining profit and writing the formula to calculate it
     return profit
 }
-console.log(`Profit: ${calculateProfit(100,150,15)}`); // Output: 750
-console.log(`Profit: ${calculateProfit(12,20,150)}`); // Output: 1200
+console.log(`Profit: $${calculateProfit(100,150,15)}`); // Profit: $750
+console.log(`Profit: $${calculateProfit(12,20,150)}`); // Profit: $1200
+
+// Task 2: Function Expression
+
+function calculateSalesTax(amount, taxRate) { // Same structure as task 1
+    let salesTax = amount * taxRate;
+    return salesTax
+}
+console.log(`Sales Tax: $${calculateSalesTax(100, 0.05)}`); // Sales Tax: $5
+console.log(`Sales Tax: $${calculateSalesTax(150, 0.1)}`); // Sales Tax: $15
